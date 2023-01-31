d0b7c3b97138584b41432c0a8ac66ea280f9f1d2
Sunnatillo Shavkatov <sunnatillo.shavkatov@udevs.io>
2023-01-31 16:13:02 +0500

    pub.dev publish 1.1.3

--- test/chuck_interceptor_test.dart
+++ test/chuck_interceptor_test.dart
@@ -1,29 +1,29 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:chuck_interceptor/chuck_interceptor.dart';
-// import 'package:chuck_interceptor/chuck_interceptor_platform_interface.dart';
-// import 'package:chuck_interceptor/chuck_interceptor_method_channel.dart';
-// import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-//
-// class MockChuckInterceptorPlatform
-//     with MockPlatformInterfaceMixin
-//     implements ChuckInterceptorPlatform {
-//
-//   @override
-//   Future<String?> getPlatformVersion() => Future.value('42');
-// }
-//
-// void main() {
-//   final ChuckInterceptorPlatform initialPlatform = ChuckInterceptorPlatform.instance;
-//
-//   test('$MethodChannelChuckInterceptor is the default instance', () {
-//     expect(initialPlatform, isInstanceOf<MethodChannelChuckInterceptor>());
-//   });
-//
-//   test('getPlatformVersion', () async {
-//     ChuckInterceptor chuckInterceptorPlugin = ChuckInterceptor();
-//     MockChuckInterceptorPlatform fakePlatform = MockChuckInterceptorPlatform();
-//     ChuckInterceptorPlatform.instance = fakePlatform;
-//
-//     expect(await chuckInterceptorPlugin.getPlatformVersion(), '42');
-//   });
-// }
+import 'package:flutter_test/flutter_test.dart';
+import 'package:chuck_interceptor/chuck_interceptor.dart';
+import 'package:chuck_interceptor/chuck_interceptor_platform_interface.dart';
+import 'package:chuck_interceptor/chuck_interceptor_method_channel.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+class MockChuckInterceptorPlatform
+    with MockPlatformInterfaceMixin
+    implements ChuckInterceptorPlatform {
+
+  @override
+  Future<String?> getPlatformVersion() => Future.value('42');
+}
+
+void main() {
+  final ChuckInterceptorPlatform initialPlatform = ChuckInterceptorPlatform.instance;
+
+  test('$MethodChannelChuckInterceptor is the default instance', () {
+    expect(initialPlatform, isInstanceOf<MethodChannelChuckInterceptor>());
+  });
+
+  test('getPlatformVersion', () async {
+    ChuckInterceptor chuckInterceptorPlugin = ChuckInterceptor();
+    MockChuckInterceptorPlatform fakePlatform = MockChuckInterceptorPlatform();
+    ChuckInterceptorPlatform.instance = fakePlatform;
+
+    expect(await chuckInterceptorPlugin.getPlatformVersion(), '42');
+  });
+}
