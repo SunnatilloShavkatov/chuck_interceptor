6023b7d8bc164fbc6e1ea91508e956ae97872a3c
mirorifmirzokirov <mirzokirovmirorif@gmail.com>
2023-09-25 09:06:19 +0500

    change chuck viewer

--- lib/ui/widget/chuck_json_viewer.dart
+++ lib/ui/widget/chuck_json_viewer.dart
@@ -418,7 +418,7 @@ class _JsonArrayViewerState extends State<JsonArrayViewer> {
         );
       }
     }
-    return const Text(
+    return const SelectableText(
       'Object',
       style: TextStyle(color: Colors.grey),
     );
