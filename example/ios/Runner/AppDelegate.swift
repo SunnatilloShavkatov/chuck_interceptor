d11d367ceefccd01d255c46475d93479993b0888
Sunnatillo Shavkatov <sunnatilloshavkatov@gmail.com>
2024-11-03 22:44:19 +0500

    package update

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,9 +8,11 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
-    if #available(iOS 10.0, *) {
-       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    // This is required to make any communication available in the action isolate.
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+       GeneratedPluginRegistrant.register(with: registry)
     }
+    UNUserNotificationCenter.current().delegate = self
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
